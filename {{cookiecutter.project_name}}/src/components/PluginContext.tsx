9e2d11b68a9ef93a49fe336236ab51fc13204d93
Dan Schuman <dan.schuman@cortex.io>
2025-05-02 10:38:14 -0700

    Update cookiecutter for new UI

--- {{cookiecutter.project_name}}/src/components/PluginContext.tsx
+++ {{cookiecutter.project_name}}/src/components/PluginContext.tsx
@@ -1,13 +1,33 @@
-import { usePluginContext } from "@cortexapps/plugin-core/components";
 import type React from "react";
 
+import { CardTitle } from "@cortexapps/react-plugin-ui";
+
+import { Heading, Section, Subsection } from "./UtilityComponents";
+import JsonView from "./JsonView";
+
+import { usePluginContextProvider } from "./PluginContextProvider";
+
 const PluginContext: React.FC = () => {
-  const context = usePluginContext();
+  const context = usePluginContextProvider();
 
   return (
-    <>
-      <pre>{JSON.stringify(context, null, 2)}</pre>
-    </>
+    <Section>
+      <Heading>
+        <CardTitle>Plugin Context</CardTitle>
+      </Heading>
+      <Subsection className="space-y-4">
+        <div>
+          Below is the plugin context object. This object is returned from the
+          usePluginContextProvider hook available in the PluginContextProvider
+          component.
+        </div>
+        {context && (
+          <div>
+            <JsonView data={context} theme={context.theme} />
+          </div>
+        )}
+      </Subsection>
+    </Section>
   );
 };
 
