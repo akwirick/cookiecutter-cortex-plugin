580edeb605744807e2cc0f38b5fe84c1e6210f0d
Aaron Wirick <aaron.wirick@cortex.io>
2026-02-12 08:04:01 -0800

    Edits

--- {{cookiecutter.project_name}}/src/api/Cortex.ts
+++
@@ -1,7 +0,0 @@
-import { CortexApi, type CortexContextResponse } from "@cortexapps/plugin-core";
-
-export const getCortexContext = async (): Promise<CortexContextResponse> => {
-  const context = await CortexApi.getContext();
-
-  return context;
-};
